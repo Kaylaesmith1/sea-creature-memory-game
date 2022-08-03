ba72431f65d8bbae9a6c7b17700cecfd1a557c5b
Kayla S <kaylaesmith1@gmail.com>
2022-08-03 09:56:48 +0000

    Layout main functions needed for game. Link background image properly.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,28 @@
-    // Get data to show cards //
+    // Card info list >> array to call in later function //
+    let cardList = [
+        {name: "starfish", img: "/assets/images/starfish-1.png"},
+        {name: "starfish", img: "/assets/images/starfish-1.png"},
+        {name: "dolphin", img: "/assets/images/dolphin-2.png"},
+        {name: "dolphin", img: "/assets/images/dolphin-2.png"},
+        {name: "octopus", img: "/assets/images/octopus-3.png"},
+        {name: "octopus", img: "/assets/images/octopus-3.png"},
+        {name: "crab", img: "/assets/images/crab-4.png"},
+        {name: "crab", img: "/assets/images/crab-4.png"},
+        {name: "stingray", img: "/assets/images/stingray-5.png"},
+        {name: "stingray", img: "/assets/images/stingray-5.png"},
+        {name: "jellyfish", img: "/assets/images/jellyfish-6.png"},
+        {name: "jellyfish", img: "/assets/images/jellyfish-6.png"},
+        {name: "whale", img: "/assets/images/whale-7.png"},
+        {name: "whale", img: "/assets/images/whale-7.png"},
+        {name: "seahorse", img: "/assets/images/seahorse-8.png"},
+        {name: "seahorse", img: "/assets/images/seahorse-8.png"},
+        //include 4 more cards for 'hard level'? //
+];
+
+// RANDOMIZE CARDS ON BOARD //
+
+// RESET GAME //
+
+// HOW TO PLAY >> included pop up instructions //
+
     
