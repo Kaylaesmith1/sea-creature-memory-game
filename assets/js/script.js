70aef7c816d0e4faf3332c95f72db2d515855cb1
Kayla S <kaylaesmith1@gmail.com>
2022-08-03 08:02:35 +0000

    Add front card images to image folder.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,2 @@
-/* <h1>Put everything in a container w the video behind it in the background and a colored container over the video? Or directly the game board?</h1>
-    <h2>Mention favicon in README</h2> */
+    // Get data to show cards //
+    
